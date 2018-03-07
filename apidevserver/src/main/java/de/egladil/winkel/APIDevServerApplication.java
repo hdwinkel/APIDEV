6613c1c0ee47fb6fb7b6e2e3dd956a7f4f782432
Detlef Winkelvoss <dewi@ikea.com>
2018-03-07 07:19:12 +0100

    configuration and health check

--- apidevserver/src/main/java/de/egladil/winkel/APIDevServerApplication.java
+++ apidevserver/src/main/java/de/egladil/winkel/APIDevServerApplication.java
@@ -1,6 +1,12 @@
 package de.egladil.winkel;
 
+import org.skife.jdbi.v2.DBI;
+
+import de.egladil.winkel.health.DatabaseHealthCheck;
 import io.dropwizard.Application;
+import io.dropwizard.db.PooledDataSourceFactory;
+import io.dropwizard.jdbi.DBIFactory;
+import io.dropwizard.migrations.MigrationsBundle;
 import io.dropwizard.setup.Bootstrap;
 import io.dropwizard.setup.Environment;
 
@@ -15,15 +21,40 @@ public class APIDevServerApplication extends Application<APIDevServerConfigurati
         return "APIDevServer";
     }
 
+    
     @Override
-    public void initialize(final Bootstrap<APIDevServerConfiguration> bootstrap) {
-        // TODO: application initialization
+    public void initialize(Bootstrap<APIDevServerConfiguration> bootstrap) {
+      bootstrap.addBundle(new MigrationsBundle<APIDevServerConfiguration>() {
+        @Override
+        public PooledDataSourceFactory getDataSourceFactory(final APIDevServerConfiguration configuration) {
+          return configuration.getDataSourceFactory();
+        }
+      });
+      // bootstrap.addBundle(new AssetsBundle("/assets/"));
     }
-
+    
+    
+    
     @Override
-    public void run(final APIDevServerConfiguration configuration,
-                    final Environment environment) {
-        // TODO: implement application
-    }
+    public void run(APIDevServerConfiguration configuration, Environment environment) throws Exception {
+      final DBIFactory factory = new DBIFactory();
+      final DBI jdbi = factory.build(environment, configuration.getDataSourceFactory(), "mysql");
 
+/*      
+      final MyDao dao = jdbi.onDemand(MyDao.class);
+
+      environment.jersey().register(new HtmlPageResource());
+      environment.jersey().register(new BrandResource(dao));
+      environment.jersey().register(new UserResource(dao));
+      environment.jersey().register(new KeywordResource(dao));
+      environment.jersey().register(new CampaignResource(dao));
+      environment.jersey().register(new CampaignHasKeywordResource(dao));
+      environment.jersey().register(new CompetitorResource(dao));
+*/
+
+      environment.healthChecks().register("health",
+          new DatabaseHealthCheck(jdbi, configuration.getDataSourceFactory().getValidationQuery()));
+    }
+    
+    
 }
--- apidevserver/src/main/java/de/egladil/winkel/APIDevServerConfiguration.java
+++ apidevserver/src/main/java/de/egladil/winkel/APIDevServerConfiguration.java
@@ -9,14 +9,38 @@ import io.dropwizard.Configuration;
 import io.dropwizard.db.DataSourceFactory;
 
 public class APIDevServerConfiguration extends Configuration {
-    // TODO: implement service configuration
+/*
+	
+	// TODO: implement service configuration
 	@Valid
     @NotNull
     @JsonProperty("database")
     private DataSourceFactory database = new DataSourceFactory();
 
+	@JsonProperty("database")
     public DataSourceFactory getDataSourceFactory() {
         return database;
     }
 	
+    @JsonProperty("database")
+    public void setDataSourceFactory(DataSourceFactory factory) {
+      this.database = factory;
+    }
+*/
+  
+	  @Valid
+	  @NotNull
+	  private DataSourceFactory database = new DataSourceFactory();
+
+	  @JsonProperty("database")
+	  public void setDataSourceFactory(DataSourceFactory factory) {
+	    this.database = factory;
+	  }
+
+	  @JsonProperty("database")
+	  public DataSourceFactory getDataSourceFactory() {
+	    return database;
+	  }
+	
+    
 }
